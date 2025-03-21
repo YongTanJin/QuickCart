60ba5b02c0a06b1bd74f2fa2d453f8edad1478e6
YongTanJin <tananan.jai@gmail.com>
2025-03-21 14:37:08 +0700

    fix sync user 3.

--- config/inngest.js
+++ config/inngest.js
@@ -11,17 +11,22 @@ export const syncUserCreation = inngest.createFunction (
         id: 'sync-user-from-clerk',
     },
     { event: 'clerk/user.created' },
-    async ({event}) => {
-        const { id, first_name, last_name, email_address, image_url } = event.data;
-        const userData = new User({
-            _id: id,
-            email: email_address[0].email_address,
-            name: first_name + ' ' + last_name,
-            imageUrl: image_url,
-        });
-        await connectDB()
-        await User.create(userData)
-    }
+    async ({event}) => 
+        {
+            const { id, first_name, last_name, email_addresses, image_url }  = event.data
+            const userData=
+            {
+                _id:id,
+                email: email_addresses[0].email_address,
+                name: first_name + ' ' + last_name,   
+                imageUrl:image_url
+
+            }
+            await connnectDB()
+            await User.create(userData)
+                      
+        } 
+
 )
 
 //inngest function to update user data in database

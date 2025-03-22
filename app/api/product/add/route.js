a61fe41297b50479c3f5850cb9e0543f0053292e
YongTanJin <tananan.jai@gmail.com>
2025-03-22 23:09:37 +0700

    Updataion & seller progress

---
+++ app/api/product/add/route.js
@@ -0,0 +1,82 @@
+import connectDB from "@/config/db";
+import authSeller from "@/lib/authSeller";
+import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { v2 as cloudinary } from "cloudinary";
+import { NextResponse } from "next/server";
+
+
+// Configuring cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secert: process.env.CLOUDINARY_API_SECRET
+})
+
+
+export async function POST(request) {
+    try {
+
+        const {userId} = getAuth()
+
+        const IsSeller = await authSeller(userId)
+
+        if (!isSeller) {
+            return NextResponse.json({ success: false, message: 'not authorized' })
+        }
+
+        const fromData = await request.feomData()
+
+        const name = fromData.get('name')
+        const description = fromData.get('description')
+        const category = fromData.get('category')
+        const price = fromData.get('price')
+        const offerPrice = fromData.get('offerPrice')
+
+        const files = fromData.get('images')
+
+        if (!files || files.length === 0) {
+            return NextResponse.json({ success: false, message: 'no file uploaded' })
+        }
+
+        const result = await Promise.all (
+            files.map(async (file) => {
+                const arrayBuffer = await file.arrayBuffer()
+                const buffer = Buffer.from(arrayBuffer)
+
+                return new Promise((resolve, reject) => {
+                    const stream = cloudinary.uploader.upload_stream(
+                        {resource_type: 'auto'},
+                        (error, result) => {
+                            if (error) {
+                                reject(error)
+                            } else{
+                                resolve(result)
+                            }
+                        }
+                    )
+                    stream.end(buffer)
+                })
+            })
+        )
+
+        const image = result.map(result => result.secure_url)
+
+        await connectDB()
+        const newProduct = await Product.create({
+            userId,
+            name,
+            description,
+            category,
+            price:Number(price),
+            offerPrice:Number(offerPrice),
+            image,
+            date: Date.now()
+        })
+
+        return NextResponse.json({ success: true, message: 'Upload successful', newProduct })
+
+    } catch (error){
+        NextResponse.json({ success: false, message: error.message })
+    }
+}
---
+++ models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true},
+    price: { type: Number, required: true},
+    offerPrice: { type: Number, required: true},
+    image: { type: String, required: true},
+    category: { type: String, required: true},
+    date: { type: Number, required: true}
+})
+
+const Product = mongoose.models.model.Product || mongoose.model('product', productSchema)
+
+export default Product

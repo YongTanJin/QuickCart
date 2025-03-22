a7cfb5dd47d6ae3a16db8d3f3719c71a950b27bc
YongTanJin <tananan.jai@gmail.com>
2025-03-23 00:33:24 +0700

    Update route.js

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -10,8 +10,8 @@ import { NextResponse } from "next/server";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 export async function POST(request) {
